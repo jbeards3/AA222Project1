057a71a35025f5e9d36ec3e6a7e93c794af68345
jbeards3 <91290502+jbeards3@users.noreply.github.com>
2022-04-06 12:44:19 -0700

    Update
    Update

--- project1_jl/plots.jl
+++ project1_jl/plots.jl
@@ -1,8 +1,13 @@
 using LaTeXStrings
 using Plots
 
-function rosenbrock(x, y)
+## Functions
+function rosenbrock_plot(x, y)
     return (1.0 - x)^2 + 100.0 * (y - x^2)^2
+end
+
+function rosenbrock(x::Vector)
+    return (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
 end
 
 function rosenbrock_gradient(x::Vector)
@@ -39,24 +44,19 @@ function powell_gradient(x::Vector)
     return storage
 end
 
-
-
-
-
-
-function optimizer_simple(f, g, x0, n, step = 0.01)
+## Optimizer Functions
+function optimizer_grad_descent(f, g, x0, n, alpha = 0.01)
     x_history = [x0]
     f_history = [f(x0)]
 
-    for i = 1:3:n-3
+    for i = 1:2:n
+        x = x_history[end]
+        g_x = g(x)
         # Calculate local descent
-        g_x = g(x_history[end])
         d = -g_x / norm(g_x) 
-        # step = argmin(f(x_history[end] + step * d))
 
         # Calculate next design point
-        x_next = x_history[end] + step * d
-        # x_next = x_history[end] - step * g_x
+        x_next = x_history[end] + alpha * d
 
         # Add design point to history of design points
         push!(x_history, x_next)
@@ -66,16 +66,16 @@ function optimizer_simple(f, g, x0, n, step = 0.01)
     return x_history, f_history
 end
 
-function optimizer_momentum(f, g, x0, n, step = 1, Beta = 0)
+function optimizer_momentum(f, g, x0, n, alpha = 0.01, beta = 0)
     x_history = [x0]
     f_history = [f(x0)]
     v = zeros(length(x0))
 
-    for i = 1:3:n-3
+    for i = 1:2:n
         # Calculate local descent
         g_x = g(x_history[end])
         d = -g_x / norm(g_x)
-        v[:] = Beta*v + step * d
+        v[:] = beta*v + alpha * d
 
         # Calculate next design point
         x_next = x_history[end] + v
@@ -88,37 +88,94 @@ function optimizer_momentum(f, g, x0, n, step = 1, Beta = 0)
     return x_history, f_history
 end
 
+function optimizer_momentum_decay(f, g, x0, n; alpha = 0.01, beta = 0, gamma = 1, p = 0.9)
+    x_history = [x0]
+    f_history = [f(x0)]
+    v = zeros(length(x0))
+
+    for i = 1:2:n
+        # Calculate local descent
+        g_x = g(x_history[end] + beta * v)
+        d = -g_x / norm(g_x)
+        v[:] = beta*v + alpha * gamma * d
+        gamma *= p
+
+        # Calculate next design point
+        x_next = x_history[end] + v
+
+        # Add design point to history of design points
+        push!(x_history, x_next)
+        push!(f_history, f(x_next))
+    end
+
+    return x_history, f_history
+end
+
+## Function Minimization
 # Rosenbrock function
-x_history_1, f_history_1 = optimizer_simple(rosenbrock, rosenbrock_gradient, [-1.0, -1.0], 20, 0.24)
-# x_history, f_history = optimizer_momentum(rosenbrock, rosenbrock_gradient, [-1.0, -1.0], 20, 1, 0.2)
+x_history_r1, f_history_r1 = optimizer_momentum_decay(rosenbrock, rosenbrock_gradient, [-1.0, -1.0], 100; alpha = 0.2, beta = 0.8, gamma = 1, p = 0.85)
+x_history_r2, f_history_r2 = optimizer_momentum_decay(rosenbrock, rosenbrock_gradient, [0, -1.0], 100; alpha = 0.2, beta = 0.8, gamma = 1, p = 0.85)
+x_history_r3, f_history_r3 = optimizer_momentum_decay(rosenbrock, rosenbrock_gradient, [-1.0, 0], 100; alpha = 0.2, beta = 0.8, gamma = 1, p = 0.85)
 
 # Himmelblau function
-x_history_2, f_history_2 = optimizer_simple(himmelblau, himmelblau_gradient, [-1.0, -1.0], 40, 0.3)
+x_history_h1, f_history_h1 = optimizer_simple(himmelblau, himmelblau_gradient, [-1.0, -1.0], 100, 0.3)
+x_history_h2, f_history_h2 = optimizer_simple(himmelblau, himmelblau_gradient, [0, -1.0], 100, 0.3)
+x_history_h3, f_history_h3 = optimizer_simple(himmelblau, himmelblau_gradient, [-1.0, 0], 100, 0.3)
 
 # Powell function
-x_history_3, f_history_3 = optimizer_simple(powell, powell_gradient, [-1.0, -1.0, -1.0, -1.0], 100, 0.1)
+x_history_p1, f_history_p1 = optimizer_simple(powell, powell_gradient, [-1.0, -1.0, -1.0, -1.0], 100, 0.1)
+x_history_p2, f_history_p2 = optimizer_simple(powell, powell_gradient, [0, 0, -1.0, -1.0], 100, 0.1)
+x_history_p3, f_history_p3 = optimizer_simple(powell, powell_gradient, [-1.0, -1.0, 0, 0], 100, 0.1)
 
-# Plotting for rosenbrock function
-# Contour of rosenbrock
+## Plotting
+# Contour of Rosenbrock
 xr = -2:0.1:2
 yr = -2:0.1:2
-contour(xr, yr, rosenbrock, levels = [10, 25, 50, 100, 200, 250, 300], colorbar = false,
+contour(xr, yr, rosenbrock_plot, levels = [1, 5, 10, 25, 50, 100, 200, 300, 400, 500], colorbar = true,
         c = cgrad(:viridis, rev = true), legend = false, xlims = (-2, 2), ylims = (-2, 2),
-        xlabel = "x₁", ylabel = "x₂", aspectratio = :equal, clim = (2, 500))
-plot!([x_history_1[i][1] for i = 1:length(x_history_1)], [x_history_1[i][2] for i = 1:length(x_history_1)], color = :black)
+        xlabel = "x₁", ylabel = "x₂", aspectratio = :equal, clim = (1, 500), title = "Contour Plot - Rosenbrock")
+plot!([x_history_r1[i][1] for i = 1:length(x_history_r1)], [x_history_r1[i][2] for i = 1:length(x_history_r1)],
+ color = :blue, label = "Starting Position: [-1.0, -1.0]")
+
+plot!([x_history_r2[i][1] for i = 1:length(x_history_r2)], [x_history_r2[i][2] for i = 1:length(x_history_r2)],
+ color = :red, label = "Starting Position: [0, -1.0]")
+ 
+plot!([x_history_r3[i][1] for i = 1:length(x_history_r3)], [x_history_r3[i][2] for i = 1:length(x_history_r3)],
+ color = :green, label = "Starting Position: [-1.0, 0]")
+
 savefig("Rosenbrock_Contour")
 
-# Convergence plot
-using Plots
-plot(collect(1:length(f_history_1)), f_history_1, xlabel = "Iteration", ylabel = "f(x)")
+# Convergence plot - Rosenbrock
+plot(collect(1:length(f_history_r1)), f_history_r1, xlabel = "Iteration", ylabel = "f(x)",
+color = :blue, label = "Starting Position: [-1.0, -1.0]", title = "Convergence Plot - Rosenbrock")
+
+plot!(collect(1:length(f_history_r2)), f_history_r2, xlabel = "Iteration", ylabel = "f(x)",
+color = :red, label = "Starting Position: [0, -1.0]")
+
+plot!(collect(1:length(f_history_r3)), f_history_r3, xlabel = "Iteration", ylabel = "f(x)",
+color = :green, label = "Starting Position: [-1.0, 0]")
+
 savefig("Rosenbrock_Convergence")
 
-# Convergence plot
-using Plots
-plot(collect(1:length(f_history_2)), f_history_2, xlabel = "Iteration", ylabel = "f(x)")
+# Convergence plot - Himmelblau
+plot(collect(1:length(f_history_h1)), f_history_h1, xlabel = "Iteration", ylabel = "f(x)",
+color = :blue, label = "Starting Position: [-1.0, -1.0]", title = "Convergence Plot - Himmelblau")
+
+plot!(collect(1:length(f_history_h2)), f_history_h2, xlabel = "Iteration", ylabel = "f(x)",
+color = :red, label = "Starting Position: [0, -1.0]")
+
+plot!(collect(1:length(f_history_h3)), f_history_h3, xlabel = "Iteration", ylabel = "f(x)",
+color = :green, label = "Starting Position: [-1.0, 0]")
 savefig("Himmelblau_Convergence")
 
-# Convergence plot
-using Plots
-plot(collect(1:length(f_history_3)), f_history_3, xlabel = "Iteration", ylabel = "f(x)")
+# Convergence plot - Powell
+plot(collect(1:length(f_history_p1)), f_history_p1, xlabel = "Iteration", ylabel = "f(x)",
+color = :blue, label = "Starting Position: [-1.0, -1.0, -1.0, -1.0]", title = "Convergence Plot - Powell")
+
+plot!(collect(1:length(f_history_p2)), f_history_p2, xlabel = "Iteration", ylabel = "f(x)",
+color = :red, label = "Starting Position: [0, 0, -1.0, -1.0]")
+
+plot!(collect(1:length(f_history_p3)), f_history_p3, xlabel = "Iteration", ylabel = "f(x)",
+color = :green, label = "Starting Position: [-1.0, -1.0, 0, 0]")
+
 savefig("Powell_Convergence")
--- project1_jl/project1.jl
+++ project1_jl/project1.jl
@@ -41,59 +41,127 @@ Returns:
     - The location of the minimum
 """
 
+# Will only be logging history of x throughout each iteration since gradient descent method doesn't need f(x),
+# plus keeping history of f(x) at each iteration wastes an evaluation, thus reducing the number of iterations
+# I can run. Since the number of evaluation doesn't matter for plotting, I will be logging f(x) history there
 function optimize(f, g, x0, n, prob)
     if prob == "simple1"
-        x_history, f_history = optimizer_simple(f, g, x0, n, 0.24)
-        #x_history, f_history = optimizer_momentum(f, g, x0, n, 1, 0.2)
+        x_history = optimizer_momentum_decay(f, g, x0, n; alpha = 0.2, beta = 0.8, gamma = 0.85)
     elseif prob == "simple2"
-        x_history, f_history = optimizer_simple(f, g, x0, n, 0.3)
-    else
-        x_history, f_history = optimizer_simple(f, g, x0, n, 0.1)
+        x_history = optimizer_momentum_decay(f, g, x0, n; alpha = 1, beta = 0.9, gamma = 0.85)
+    elseif prob == "simple3"
+        x_history = optimizer_momentum_decay(f, g, x0, n; alpha = 1, beta = 0.9, gamma = 0.85)
+    elseif prob == "secret1"
+        x_history = optimizer_momentum_decay(f, g, x0, n; alpha = 1, beta = 0.9, gamma = 0.85)
+    elseif prob == "secret2"
+        x_history = optimizer_momentum_decay(f, g, x0, n; alpha = 1, beta = 0.9, gamma = 0.85)
     end
-    x_best = x_history[argmin(f_history)]
+    x_best = last(x_history)
     return x_best
 end
 
-function optimizer_simple(f, g, x0, n, step = 0.01)
+function optimizer_grad_descent(f, g, x0, n, alpha = 0.01, beta = 0.0001)
     x_history = [x0]
-    f_history = [f(x0)]
 
-    while count(f, g) < n - 1
+    while count(f, g) < n
+
+        x = x_history[end]
+        # f_x = f(x)
+        g_x = g(x)
+
         # Calculate local descent
-        g_x = g(x_history[end])
-        d = -g_x / norm(g_x) 
-        # step = argmin(f(x_history[end] + step * d))
+        d = -g_x / norm(g_x)
+
+        # Backtracking Line search
+        # while f(x + alpha * d) > f_x + beta * alpha * (g_x⋅d)
+        #     alpha *= 0.5
+        # end
 
         # Calculate next design point
-        x_next = x_history[end] + step * d
+        x_next = x_history[end] + alpha * d
         # x_next = x_history[end] - step * g_x
 
         # Add design point to history of design points
         push!(x_history, x_next)
-        push!(f_history, f(x_next))
     end
 
-    return x_history, f_history
+    return x_history
 end
 
-function optimizer_momentum(f, g, x0, n, step = 1, Beta = 0)
+function optimizer_momentum(f, g, x0, n, alpha = 0.01, beta = 0)
     x_history = [x0]
-    f_history = [f(x0)]
     v = zeros(length(x0))
 
-    for i = 1:3:n-3
+    while count(f, g) < n
         # Calculate local descent
         g_x = g(x_history[end])
         d = -g_x / norm(g_x)
-        v[:] = Beta*v + step * d
+        v[:] = beta*v + alpha * d
 
         # Calculate next design point
         x_next = x_history[end] + v
 
         # Add design point to history of design points
         push!(x_history, x_next)
-        push!(f_history, f(x_next))
     end
 
-    return x_history, f_history
+    return x_history
+end
+
+function optimizer_momentum_decay(f, g, x0, n; alpha = 0.1, beta = 0.9, gamma = 0.75)
+    x_history = [x0]
+    v = zeros(length(x0))
+
+    while count(f, g) < n
+        # Calculate gradient
+        g_x = g(x_history[end] + beta * v)
+
+        # Calculate local descent
+        d = -g_x / norm(g_x)
+
+        # Calculate momentum
+        v[:] = beta * v + alpha * d
+
+        # Calculate next design point
+        x_next = x_history[end] + v
+
+        # Add design point to history of design points
+        push!(x_history, x_next)
+
+        # Decay the learning rate each step
+        alpha *= gamma
+    end
+
+    return x_history
+end
+
+function optimizer_ADAM(f, g, x0, n; alpha = .001, gamma_v = 0.9, gamma_s = 0.999, eps = 1E-8)
+    x_history = [x0]
+    v = zeros(length(x0))
+    s = zeros(length(x0))
+
+    while count(f, g) < n
+        # Calculate gradient
+        g_x = g(x_history[end])
+
+        # Update biased decaying momentum
+        v[:] = gamma_v * v + (1 - gamma_v) * g_x
+
+        # Update biased decaying square gradient
+        s[:] = gamma_s * s + (1 - gamma_s) * (g_x .* g_x)
+
+        # Calculate corrected decaying momentum
+        v_c = v ./ (1 - gamma_v)
+
+        # Calculate corrected decaying square gradient
+        s_c = s ./ (1 - gamma_s)
+
+        # Calculate next design point
+        x_next = x_history[end] - (alpha * v_c) ./ (sqrt.(s_c) .+ eps)
+
+        # Add design point to history of design points
+        push!(x_history, x_next)
+    end
+
+    return x_history
 end
